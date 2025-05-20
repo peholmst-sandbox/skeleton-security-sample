508f9200f8cb7bd5f9b0de812eef960580a7c1be
start.vaadin.com <no-reply@vaadin.com>
2025-05-20 07:04:40 +0000

    Generated project

---
+++ src/main/java/com/example/skeletonsecurity/base/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable domain classes.
+ */
+@NullMarked
+package com.example.skeletonsecurity.base.domain;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/com/example/skeletonsecurity/base/ui/component/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable UI components.
+ */
+@NullMarked
+package com.example.skeletonsecurity.base.ui.component;
+
+import org.jspecify.annotations.NullMarked;
